664936d13618e724b5a1833b41587091dc610cc3
Adrian <z5242789@ad.unsw.edu.au>
2025-10-11 22:17:24 +1100

    H18FCAKE-20 As a front end developer, i want to have a list of API from back end, so that i can use them to build an application for website.

--- backend/app/routers/courses.py
+++ backend/app/routers/courses.py
@@ -1,17 +1,19 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
+
+from .. import models
 from ..db import get_db
-from app import model, schemas
+from app import schemas
 from ..deps import require_role
 
 router = APIRouter(prefix="/v1/courses", tags=["courses"])
 
 @router.post("/", response_model=schemas.CourseOut, dependencies=[Depends(require_role("admin"))])
 def create_course(payload: schemas.CourseIn, db: Session = Depends(get_db)):
-    c = model.Course(**payload.model_dump())
+    c = models.Course(**payload.model_dump())
     db.add(c); db.commit(); db.refresh(c)
     return c
 
 @router.get("/", response_model=list[schemas.CourseOut])
 def list_courses(db: Session = Depends(get_db)):
-    return db.query(model.Course).all()
+    return db.query(models.Course).all()
