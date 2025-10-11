641f6a579e07be948afe0815e9c13dc67b58ca9d
Peng Zhang <auguszp@163.com>
2025-10-11 08:50:28 +0800

    H18FCAKE-3  Added viewpages in routes

--- frontend/src/routes.jsx
+++ frontend/src/routes.jsx
@@ -4,6 +4,7 @@ import CoursesPage from "./pages/course";
 import FileUpload from "./pages/fileupload";
 import { AuthProvider } from "./context/auth-context";
 import { Toaster } from "sonner";
+import ViewPages from "./pages/viewmpages";
 
 export default function PageRoutes() {
     return (
@@ -13,6 +14,7 @@ export default function PageRoutes() {
                     <Route path="/" element={<LoginMain />} />
                     <Route path="/courses" element={<CoursesPage />} />
                     <Route path="/fileupload" element={<FileUpload />} />
+                    <Route path="/viewmpages" element={<ViewPages />} />
                 </Routes>
                 <Toaster position="top-right" richColors closeButton />
             </>
