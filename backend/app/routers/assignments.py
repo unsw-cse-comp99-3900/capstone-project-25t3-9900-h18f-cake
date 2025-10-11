919c8dd55b0b03812cd667fe1f15176a24543e0d
Adrian <z5242789@ad.unsw.edu.au>
2025-10-11 22:17:24 +1100

    H18FCAKE-20 As a front end developer, i want to have a list of API from back end, so that i can use them to build an application for website.

--- backend/app/routers/assignments.py
+++ backend/app/routers/assignments.py
@@ -1,22 +1,22 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from ..db import get_db
-from .. import model, schemas
+from .. import models, schemas
 from ..deps import require_role
 
 router = APIRouter(prefix="/v1/assignments", tags=["assignments"])
 
 @router.post("/", response_model=schemas.AssignmentOut, dependencies=[Depends(require_role("admin","coordinator"))])
 def create_assignment(payload: schemas.AssignmentIn, db: Session = Depends(get_db)):
-    if not db.get(model.Course, payload.course_id):
+    if not db.get(models.Course, payload.course_id):
         raise HTTPException(status_code=400, detail="Course not found")
-    a = model.Assignment(**payload.model_dump())
+    a = models.Assignment(**payload.model_dump())
     db.add(a); db.commit(); db.refresh(a)
     return a
 
 @router.get("/", response_model=list[schemas.AssignmentOut])
 def list_assignments(course_id: int | None = None, db: Session = Depends(get_db)):
-    q = db.query(model.Assignment)
+    q = db.query(models.Assignment)
     if course_id:
-        q = q.filter(model.Assignment.course_id == course_id)
+        q = q.filter(models.Assignment.course_id == course_id)
     return q.all()
